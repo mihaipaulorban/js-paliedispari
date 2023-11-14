0cacf596a84248d49413c8aa4ea571a218f9b0ea
mihaipaulorban <mihaipaulorban@gmail.com>
2023-11-14 15:13:11 +0100

    feat:esercizio parole polindrome

---
+++ Polidroma/scripti.js
@@ -0,0 +1,39 @@
+/*
+Suggerimenti di parole polindrome
+radar
+livello
+anna
+otto
+osso
+riconoscer
+rotor
+civic
+annona
+elle
+*/
+
+// Creo la funzione
+function isPalindroma(parola) {
+    // Converti la parola in minuscolo per rendere il confronto senza distinzione tra maiuscole e minuscole
+    parola = parola.toLowerCase();
+
+    // Inverti la parola
+    var parolaInvertita = parola.split('').reverse().join('');
+
+    // Confronta la parola originale con quella invertita
+    if (parola === parolaInvertita) {
+        return true; // La parola è palindroma
+    } else {
+        return false; // La parola non è palindroma
+    }
+}
+
+// Passo 1: Richiedere all'utente di inserire una parola
+var parolaUtente = prompt("Inserisci una parola:");
+
+// Passo 3: Restituire il risultato all'utente
+if (isPalindroma(parolaUtente)) {
+    console.log(parolaUtente + " è una parola palindroma!");
+} else {
+    console.log(parolaUtente + " non è una parola palindroma.");
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Esercizi in Js
+
+## Policroma
+
+In questo esercizio bisogna richiedere all'utente una parola, analizzarla e capire se é policroma o no
+
+Chiedo all'utente di srivere la parola, tramite prompt
+
+Creo una funzione per capire se la parola è palindroma:
+
+## La funzione
+
+1-Scrivi una funzione che ha una stringa come argomento
+2-All'interno di questa funzione, inverto la stringa.
+3-Confronto la stringa originale con la sua versione invertita.
+4-Se sono uguali, la parola è palindroma
+
+Restituisco quindi il risultato all'utente:
+
+Stampo un messaggio che informi l'utente se la parola inserita è palindroma o meno.
